750e90b162a65ac8198048c884784e2171f2bf5b
Alym <alymyrysmamatov@gmail.com>
2024-11-20 18:16:59 +0600

    Information

---
+++ src/PersonInfo.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const PersonInfo = () => {
+    return (
+        <div>
+            <h1>Alym Yrysmamatov</h1>
+            <p>Должность: Junior Developer</p>
+            <p>Компания: Mega</p>
+            <ul>
+                <li>Опыт: 2 года</li>
+                <li>Образование: Бакалавр информатики</li>
+                <li>Навыки: React, Node.js, Java</li>
+            </ul>
+        </div>
+    );
+};
+
+export default PersonInfo;
